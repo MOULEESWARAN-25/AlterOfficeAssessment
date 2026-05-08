2b8e226681982da77907dc19fea308dbe8bb9bde
MOULEESWARAN-25 <strangedr965@gmail.com>
2026-05-08 18:35:12 +0530

    changes in frontend-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Login';
-import Signup from './SignUp';
-import TodoDashboard from './TodoDashboard';
+import LoginPage from './Login';
+import SignupPage from './SignUp';
+import TodoDashboardPage from './TodoDashboard';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<TodoDashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<TodoDashboardPage />} />
         <Route path="/" element={<Navigate to="/dashboard" />} />
       </Routes>
     </Router>
